47054f1bb21c9224686b4ad4b565430a5b79b687
xianii <xianyi.xia@outlook.com>
2024-06-16 03:39:52 +0800

    add guessPlatform function

--- src/decorators/onebot.ts
+++ src/decorators/onebot.ts
@@ -1,12 +1,8 @@
 import {Element, Session, h} from "koishi";
 
 function Middleware(session: Session) {
-	let platform = session.platform;
-	if (session.event.user && session.event.user.avatar) {
-		if (session.event.user.avatar.includes("q.qlogo.cn")) {
-			platform = "QQ";
-		}
-	}
+	const platform = guessPlatform(session);
+
 	let head: Element[] = [
 		h("b", `${session.username}`),
 		h("span", " 转发自 "),
@@ -14,6 +10,15 @@ function Middleware(session: Session) {
 		h("span", `：`),
 	];
 	return {head: head, content: session.elements};
+}
+
+function guessPlatform(session: Session) {
+	if (session.event.user && session.event.user.avatar) {
+		if (session.event.user.avatar.includes("q.qlogo.cn")) {
+			return "QQ";
+		}
+	}
+	return session.platform;
 }
 
 export default {
