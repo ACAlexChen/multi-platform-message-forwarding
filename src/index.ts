e83fc964805c59d54c501542ff81b0d1adffe1ae
xianii <xianyi.xia@outlook.com>
2024-06-16 18:02:32 +0800

    优化文档与配置

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,18 @@ export const reusable = true;
 // export const inject = {
 // 	optional: ["cache"],
 // };
+export const name = `forward hime - 转发姬`;
+const version = `<sub>v1.1.1</sub>`;
+export const usage = `
+# ${name} ${version}
+## 配置说明
+1. 添加 \`互通转发组\`
+2. 为添加的 \`互通转发组\` 配置转发节点。每个转发节点对应某个平台的一个频道或群组
+## 特性
+- 可以配置多个 \`互通转发组\`，每个 \`互通转发组\` 之间相互独立
+- 启用插件后，当一个 \`互通转发组\` 中的任意节点收到消息时，插件会自动将消息转发给**这个**\`互通转发组\`中的其他节点。
+---`;
 
-export const usage = `Test usage`;
 export const Config = createConfig();
 
 // declare module "@koishijs/cache" {
@@ -25,13 +35,13 @@ export const Config = createConfig();
 export function apply(ctx: Context, cfg: ConfigSet) {
 	ctx.on("message-created", async (session) => {
 		const hitGroup = [];
-		for (const g in cfg.Forward_Groups) {
-			const group = cfg.Forward_Groups[g];
-			for (const k in group.Forward_Node) {
+		for (const g in cfg.ForwardGroups) {
+			const group = cfg.ForwardGroups[g];
+			for (const k in group.Nodes) {
 				if (
-					group.Forward_Node[k].Guild === session.channelId &&
-					group.Forward_Node[k].Platform === session.platform &&
-					group.Forward_Node[k].BotID !== session.userId
+					group.Nodes[k].Guild === session.channelId &&
+					group.Nodes[k].Platform === session.platform &&
+					group.Nodes[k].BotID !== session.userId
 				) {
 					logger.info("[message-created]", session.messageId, session.elements);
 					hitGroup.push(g);
@@ -40,10 +50,10 @@ export function apply(ctx: Context, cfg: ConfigSet) {
 			}
 		}
 		for (const g in hitGroup) {
-			const group = cfg.Forward_Groups[hitGroup[g]];
-			for (const k in group.Forward_Node) {
-				if (group.Forward_Node[k].Guild !== session.channelId) {
-					MessageForward(ctx, group.Forward_Node[k], session);
+			const group = cfg.ForwardGroups[hitGroup[g]];
+			for (const k in group.Nodes) {
+				if (group.Nodes[k].Guild !== session.channelId) {
+					MessageForward(ctx, group.Nodes[k], session);
 				}
 			}
 		}
